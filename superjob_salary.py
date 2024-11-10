1e52c25c54941a6368d901532fb24a4135059ef5
Maxim L. <38497831+StableBig@users.noreply.github.com>
2024-11-11 00:15:04 +0100

    Get job titles

--- superjob_salary.py
+++ superjob_salary.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import time
 from dotenv import load_dotenv
 
 
@@ -9,72 +8,27 @@ load_dotenv()
 SUPERJOB_API_KEY = os.getenv("SUPERJOB_API_KEY")
 
 
-def predict_rub_salary(vacancy):
-    if vacancy.get("currency") == "rub":
-        min_salary = vacancy.get("payment_from")
-        max_salary = vacancy.get("payment_to")
-
-        if min_salary and max_salary:
-            return (min_salary + max_salary) / 2
-        elif min_salary:
-            return min_salary * 1.2
-        elif max_salary:
-            return max_salary * 0.8
-    return None
-
-
-def get_superjob_vacancies(language):
+def get_vacancy_titles():
     url = "https://api.superjob.ru/2.0/vacancies/"
     headers = {
         "X-Api-App-Id": SUPERJOB_API_KEY
     }
-    all_salaries = []
-    page = 0
 
-    while True:
-        params = {
-            "keyword": f"Программист {language}",
-            "town": "Москва",
-            "page": page
-        }
-
-        response = requests.get(url, headers=headers, params=params)
-
-        if response.status_code != 200:
-            print(f"Ошибка при запросе для {language}, страница {page}: {response.status_code}")
-            break
-
-        vacancies_page = response.json()
-        vacancies = vacancies_page.get("objects", [])
-
-        if not vacancies:
-            break
-
-        print(f"Загружаю {language}, страница {page + 1}")
-
-        for vacancy in vacancies:
-            salary = predict_rub_salary(vacancy)
-            if salary:
-                all_salaries.append(salary)
-
-        page += 1
-        time.sleep(0.2)
-
-    vacancies_found = vacancies_page.get("total")
-    vacancies_processed = len(all_salaries)
-    average_salary = int(sum(all_salaries) / vacancies_processed) if vacancies_processed > 0 else None
-
-    return {
-        "vacancies_found": vacancies_found,
-        "vacancies_processed": vacancies_processed,
-        "average_salary": average_salary
+    params = {
+        "town": "Москва",
+        "count": 20
     }
 
+    response = requests.get(url, headers=headers, params=params)
 
-languages = ["Python", "Java", "JavaScript", "C++", "C#", "Ruby", "PHP", "Swift", "Go", "Kotlin"]
-superjob_statistics = {}
+    if response.status_code == 200:
+        vacancies = response.json().get("objects", [])
 
-for language in languages:
-    superjob_statistics[language] = get_superjob_vacancies(language)
+        # Выводим названия вакансий
+        for vacancy in vacancies:
+            print(vacancy.get("profession"))
+    else:
+        print(f"Ошибка при запросе: {response.status_code}")
 
-print(superjob_statistics)
+
+get_vacancy_titles()
